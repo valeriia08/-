a26314b31dc77a8e3eba7ce301f23caf9d373ed0
valeriia08 <130174093+valeriia08@users.noreply.github.com>
2023-04-07 21:21:01 +0300

    Update code1.c

--- code1.c
+++ code1.c
@@ -1,22 +1,23 @@
-/*Дана..*/
+/*Дана програма вираховує гіпотенузу та периметр трикутника*/
 #include<stdio.h> 
 #include<math.h>
 #include<locale.h>
+
 int main() {
 setlocale (LC_CTYPE, "ua");
-int a, b;
-printf("Введіть значення a:");
-scanf("%d", &a);
-printf("Введіть значення b:");
-scanf("%d", &b);
-float c=sqrt(a*a+b*b);
+    
+int a, b; //Оголошуємо змінні
+printf("Введіть значення a та b:"); 
+scanf("%d %d" , &a, &b  ); //Введення змінних
 
+float c=sqrt(a*a+b*b); //знаходження гіпотенузи
+int p=a+b+c; //знаходження периметру
+    
 printf("Гіпотенуза дорівнює %.2f\n", c);
-int p=a+b+c;
-printf("Периметр дорівнює %d\n", p);
+printf("Периметр дорівнює %d\n", p); //Виведення результату
 
 
 
 
-    return 0;
+    return 0; //кінець програми
 }
