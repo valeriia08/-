4a71ed6e871bc6b64eeb18c3c74ee1749ebc2a6f
valeriia08 <130174093+valeriia08@users.noreply.github.com>
2023-04-07 21:08:45 +0300

    Update code1.c

--- code1.c
+++ code1.c
@@ -1,5 +1,22 @@
-#include<stdio.h>
+/*Дана..*/
+#include<stdio.h> 
+#include<math.h>
+#include<locale.h>
 int main() {
-    printf("hello world");
+setlocale (LC_CTYPE, "ua");
+int a, b;
+printf("Введіть значення a:");
+scanf("%d", &a);
+printf("Введіть значення b:");
+scanf("%d", &b);
+float c=sqrt(a*a+b*b);
+
+printf("Гіпотенуза дорівнює %.2f\n", c);
+int p=a+b+c;
+printf("Периметр дорівнює %d\n", p);
+
+
+
+
     return 0;
 }
